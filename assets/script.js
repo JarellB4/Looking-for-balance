e182ba8d32d0fa3f28545bbb7640ea4d4aae4e44
Sara Taylor <sara.swifti@gmail.com>
2021-01-16 12:05:54 -0500

    first push

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 //starter code to test for API
 const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://type.fit/api/quotes",
-    "method": "GET"
-  }
+  "async": true,
+  "crossDomain": true,
+  "url": "https://type.fit/api/quotes",
+  "method": "GET"
+}
+
+$.ajax(settings).done(function (response) {
+  const data = JSON.parse(response);
+  console.log(data);
+  var inspiration = data;
+  var whenPushed = $("<p>").text(inspiration);
+  $("header").append(whenPushed);
+
+
+
   
-  $.ajax(settings).done(function (response) {
-    const data = JSON.parse(response);
-    console.log(data);
-  });
+});
