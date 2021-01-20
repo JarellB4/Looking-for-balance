73ae929ce67462355017a30db13721c6db9e272d
Sara Taylor <sara.swifti@gmail.com>
2021-01-20 18:41:04 -0500

    finally got a working api

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,23 @@
 //starter code to test for API
-const settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://type.fit/api/quotes",
-  "method": "GET"
-}
+
+
+	const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "4186ebe167msh5543f6c878fb176p17fd99jsn82889e1748ce",
+		"x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com"
+	}
+};
 
 $.ajax(settings).done(function (response) {
-  const data = JSON.parse(response);
-  console.log(data);
-  var inspiration = data;
-  var whenPushed = $("<p>").text(inspiration);
-  $("header").append(whenPushed);
+	console.log(response);
 
-
-
-  
+  var inspiration = $("<p>").text("'" + response.text + "'- " + response.author);
+  // not necessaryinspiration.addId("inspirTag");
+ $("#header").append(inspiration);
 });
+  
+  
