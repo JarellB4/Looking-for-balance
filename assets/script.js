b5a144736872239a122b3e626ba419ef1ede9c80
Sara Taylor <sara.swifti@gmail.com>
2021-01-20 21:54:59 -0500

    adding the ajax

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,29 @@
 
 $(document).ready(function() {
 
+
+
+//starter code to test for API
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "4186ebe167msh5543f6c878fb176p17fd99jsn82889e1748ce",
+		"x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+
+  var inspiration = $("<p>").text("'" + response.text + "'- " + response.author);
+
+ $("#apiins").append(inspiration);
+});
+
+
   $("header").append("<h1> Looking for balance </h1>")
 
     // Carousel link
@@ -63,28 +86,6 @@ $(document).ready(function() {
 
 
 
-//starter code to test for API
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "4186ebe167msh5543f6c878fb176p17fd99jsn82889e1748ce",
-		"x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-
-  var inspiration = $("<p>").text("'" + response.text + "'- " + response.author);
-
- $("#apiins").append(inspiration);
-});
   
-  $.ajax(settings).done(function (response) {
-    const data = JSON.parse(response);
-    console.log(data);
-  });
- main
+  
+ 
